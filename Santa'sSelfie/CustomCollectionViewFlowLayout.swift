79274a832fde43972da3f308b1065068d08dac7c
Roman Salazar <romansito18@gmail.com>
2016-11-04 15:57:40 -0700

    added a customFlowLayout

--- Santa'sSelfie/CustomCollectionViewFlowLayout.swift
+++ Santa'sSelfie/CustomCollectionViewFlowLayout.swift
@@ -11,36 +11,35 @@ import UIKit
 class CustomCollectionViewFlowLayout:
 UICollectionViewFlowLayout {
     
-//    var columns: Int
-//    let spacing: CGFloat = 1.0
-//    
-//    var screenWidth: CGFloat {
-//        return UIScreen.main.bounds.width // represents the screen width
-//    }
-//    
-//    var itemWidth: CGFloat {
-//        let availableWidth = screenWidth - (CGFloat(self.columns) * spacing) // each item will have 1.0 point spacing b/w them
-//        return availableWidth / CGFloat(columns)
-//    }
-//    
-//    var itemHeight: CGFloat {
-//        return itemWidth
-//    }
-//    
-//    init(columns: Int) {
-//        self.columns = columns
-//        
-//        super.init()
-//        
-//        self.minimumLineSpacing = spacing
-//        self.minimumInteritemSpacing = spacing
-//        
-//        self.itemSize = CGSize(width: itemWidth, height: itemHeight)
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
+    var columns : Int
+    let spacing: CGFloat = 1.0
+    
+    var screenWidth : CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    var itemWidth: CGFloat {
+        let availableWidth = screenWidth - (CGFloat(self.columns) * spacing)
+        return availableWidth / CGFloat(columns)
+    }
+    
+    var itemHeight : CGFloat {
+        return itemWidth * 2
+    }
+
+    init(columns: Int) {
+        self.columns = columns
+        
+        super.init()
+        
+        self.minimumLineSpacing = spacing
+        self.minimumInteritemSpacing = spacing
+        
+        self.itemSize - CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
