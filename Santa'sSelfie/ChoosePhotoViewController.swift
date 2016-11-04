d66fb05281321a5620acba5acc7f0e099a548137
Roman Salazar <romansito18@gmail.com>
2016-11-04 15:41:19 -0700

    added an initial view controller and a collection view.

---
+++ Santa'sSelfie/ChoosePhotoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChoosePhotoViewController.swift
+//  Santa'sSelfie
+//
+//  Created by Roman Salazar on 11/4/16.
+//  Copyright © 2016 Roman Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ChoosePhotoViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
---
+++ Santa'sSelfie/CustomCollectionViewFlowLayout.swift
@@ -0,0 +1,46 @@
+//
+//  CustomCollectionViewFlowLayout.swift
+//  Santa'sSelfie
+//
+//  Created by Roman Salazar on 11/4/16.
+//  Copyright © 2016 Roman Salazar. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewFlowLayout:
+UICollectionViewFlowLayout {
+    
+//    var columns: Int
+//    let spacing: CGFloat = 1.0
+//    
+//    var screenWidth: CGFloat {
+//        return UIScreen.main.bounds.width // represents the screen width
+//    }
+//    
+//    var itemWidth: CGFloat {
+//        let availableWidth = screenWidth - (CGFloat(self.columns) * spacing) // each item will have 1.0 point spacing b/w them
+//        return availableWidth / CGFloat(columns)
+//    }
+//    
+//    var itemHeight: CGFloat {
+//        return itemWidth
+//    }
+//    
+//    init(columns: Int) {
+//        self.columns = columns
+//        
+//        super.init()
+//        
+//        self.minimumLineSpacing = spacing
+//        self.minimumInteritemSpacing = spacing
+//        
+//        self.itemSize = CGSize(width: itemWidth, height: itemHeight)
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+
+}
