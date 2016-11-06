24c564aea0a2a4ac5421c19fcea1d2b3bd8e2306
Roman Salazar <romansito18@gmail.com>
2016-11-05 17:51:19 -0700

    cells are working!!!!

--- Santa'sSelfie/ChoosePhotoViewController.swift
+++ Santa'sSelfie/ChoosePhotoViewController.swift
@@ -16,8 +16,37 @@ class ChoosePhotoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionViewSetup()
     }
 
 
+    func collectionViewSetup() {
+        collectionView.backgroundColor = .red
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = CustomCollectionViewFlowLayout(columns: 2)
+        collectionView.register(SantaCollectionViewCell.self, forCellWithReuseIdentifier: SantaCollectionViewCell.identifier())
+    }
 
+}
+
+
+extension ChoosePhotoViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SantaCollectionViewCell.identifier(), for: indexPath) as! SantaCollectionViewCell
+        cell.backgroundColor = .white
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    
+}
+
+extension ChoosePhotoViewController: UICollectionViewDelegate {
+    
 }
---
+++ Santa'sSelfie/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  Santa'sSelfie
+//
+//  Created by Roman Salazar on 11/4/16.
+//  Copyright © 2016 Roman Salazar. All rights reserved.
+//
+
+import UIKit
+
+extension UIResponder {
+
+    static func identifier() -> String {
+        return String(describing: self)
+    }
+    
+}
